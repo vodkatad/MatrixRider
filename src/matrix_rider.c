abc88ba7bf7d321f469b543afac1a22ddb588a56
Elena Grassi <grassi.e@gmail.com>
2015-01-07 17:00:27 +0100

    Code from matrix_rider.c

--- src/matrix_rider.c
+++ src/matrix_rider.c
@@ -1,10 +1,9 @@
 #include <R.h>
 #include <Rinternals.h>
 #include <Rdefines.h>
+#include "total_affinity.h"
 
 SEXP get_occupancy(SEXP);
-int factorial(int);
-int rec_fact(int);
 
 SEXP get_occupancy(SEXP pwm) 
 {
@@ -30,4 +29,259 @@ SEXP get_occupancy(SEXP pwm)
    p[2] = (int)(REAL(mat)[i*nrow+j]); // access element at row i and col j (0 based)
    UNPROTECT(1);
    return(res);
+}
+
+void get_matrixes(run info)
+{
+   if (alloc_matrixes(&(info->matrixes)))
+		info->error = MEMORY_ERROR;
+	matrix_ll *matrixes = info->matrixes;
+	char name[MAX_MATRIX_NAME];
+	char old_name[MAX_MATRIX_NAME];
+	double acgt[BASES];
+	strcpy(old_name, "");
+	strcpy(name, "");
+	int length = 0;
+	int ongoing = 1;
+	int i = 0;
+	while (ongoing) {
+		if (fscanf(info->f_matrixes, "%s\t%*d\t%lf\t%lf\t%lf\t%lf\n",
+			   name, &acgt[A], &acgt[C], &acgt[G], &acgt[T]) != 5) {
+#ifdef DEBUG
+			printf("You fool fscanf!\n");
+#endif				/* DEBUG */
+			if (fgetc(info->f_matrixes) != EOF)
+				info->error = MATRIX_FILE_ERROR;
+			ongoing = 0;
+			break;	/* XXX BAD */
+		}
+		if (strcmp(name, old_name)) {	/* we have a new matrix */
+#ifdef DEBUG
+			printf("Last was %d, now adding %s\n", length, name);
+#endif				/* DEBUG */
+			if (info->n_matrixes != 0) {
+				(*matrixes)->length = length;	/* assign length to last matrix */
+				get_fractions_from_pcounts(*matrixes, info);
+				check_error(info);
+				matrixes++;	/* next matrix to be loaded */
+			}
+			info->n_matrixes++;
+			length = 0;
+			strcpy((*matrixes)->name, name);
+		}
+		for (i = 0; i < BASES; i++)
+			(*matrixes)->freq[length][i] = acgt[i];
+		length++;
+		if (info->n_matrixes >= MAX_MATRIXES
+		    || length >= MAX_MATRIX_LENGTH) {
+			ongoing = 0;
+			info->error = MATRIX_LIMIT_ERROR;
+		}
+		strcpy(old_name, name);
+	}
+	(*matrixes)->length = length;	/* assign length to last matrix */
+	get_fractions_from_pcounts(*matrixes, info);
+}
+
+/*
+    Function: alloc_matrixes
+    
+    Creates space (calloc) for all the matrixes that will be loaded.
+    
+    Parameters:
+        m - matrix_ll ** where the matrixes will be stored.
+    
+    Returns:
+        ERROR (1) if any calloc call failed, OK (0) otherwise.
+*/
+int alloc_matrixes(matrix_ll ** m)
+{
+   *m = (matrix_ll *) calloc(MAX_MATRIXES + 1, sizeof(matrix_ll));
+	if (!(*m)) {
+		free(*m);
+		return ERROR;
+	}
+	int i = 0;
+	for (i = 0; i < MAX_MATRIXES; i++) {
+		(*m)[i] = (matrix_ll) calloc(1, sizeof(struct matrix_ll_));
+		if (!((*m)[i])) {
+			free_matrixes(*m, i);
+			return ERROR;
+		}
+		(*m)[i]->ll =
+		    (double **)calloc(MAX_MATRIX_LENGTH + 1, sizeof(double *));
+		if (!((*m)[i]->ll)) {
+			free_matrixes(*m, i);
+			return ERROR;
+		}
+		(*m)[i]->llrc =
+		    (double **)calloc(MAX_MATRIX_LENGTH + 1, sizeof(double *));
+		if (!((*m)[i]->llrc)) {
+			free_matrixes(*m, i);
+			return ERROR;
+		}
+		(*m)[i]->freq =
+		    (double **)calloc(MAX_MATRIX_LENGTH + 1, sizeof(double *));
+		if (!((*m)[i]->freq)) {
+			free_matrixes(*m, i);
+			return ERROR;
+		}
+		double **cur = NULL;
+		for (cur = (*m)[i]->ll; cur < (*m)[i]->ll + MAX_MATRIX_LENGTH;
+		     cur++) {
+			*cur = (double *)calloc(BASES+1, sizeof(double));
+			if (!(*cur)) {
+				free_matrixes(*m, i);
+				return ERROR;
+			}
+		}
+		for (cur = (*m)[i]->llrc; cur < (*m)[i]->llrc + MAX_MATRIX_LENGTH;
+		     cur++) {
+			*cur = (double *)calloc(BASES+1, sizeof(double));
+			if (!(*cur)) {
+				free_matrixes(*m, i);
+				return ERROR;
+			}
+		}
+		for (cur = (*m)[i]->freq;
+		     cur < (*m)[i]->freq + MAX_MATRIX_LENGTH; cur++) {
+			*cur = (double *)calloc(BASES, sizeof(double));
+			if (!(*cur)) {
+				free_matrixes(*m, i);
+				return ERROR;
+			}
+		}
+		(*m)[i]->name = (char *)calloc(MAX_MATRIX_NAME, sizeof(char));
+		if (!((*m)[i]->name)) {
+			free_matrixes(*m, i);
+			return ERROR;
+		}
+	}
+	return OK;
+}
+
+/*
+    Function: free_matrixes
+    
+        Frees the space used by all the loaded matrixes.
+    
+    Parameters:
+    
+        m - matrix_ll * with the matrixes to be freed.
+        loaded - number of matrixes to be freed.
+*/
+void free_matrixes(matrix_ll * m, int loaded)
+{
+	int i = 0;
+	for (i = 0; i < loaded; i++) {
+		free(m[i]->name);
+		double **cur = NULL;
+		for (cur = m[i]->ll; cur < m[i]->ll + MAX_MATRIX_LENGTH; cur++)
+			free(*cur);
+		free(m[i]->ll);
+		for (cur = m[i]->llrc; cur < m[i]->llrc + MAX_MATRIX_LENGTH; cur++)
+			free(*cur);
+		free(m[i]->llrc);
+		for (cur = m[i]->freq; cur < m[i]->freq + MAX_MATRIX_LENGTH; cur++)
+			free(*cur);
+		free(m[i]->freq);
+		free(m[i]);
+	}
+	free(m);
+}
+
+/*
+    Function: matrix_little_window_tot
+    
+    Computes affinity between a matrix and a string on all viable positions and
+    returns the sum of all affinities.
+    
+    Parameters:
+    
+        m   - matrix_ll matrix that will be used.
+        seq - sequence versus which matrix will be compared.
+   	s_length - the
+
+    
+    Returns:
+       
+       double with total affinity between the given matrix and fasta,
+       normalized upon the number of matrix matches != 0 (i.e. without an N).
+*/
+double matrix_little_window_tot(matrix_ll m, fasta f, int begin, run info)
+{
+	int offset = 0;
+    int *seq = f->seq;
+	double tot = 0;
+	while (offset <= f->length - m->length) {
+		tot += info->get_affinities_pointer(m, seq, offset);
+		offset++;
+	}
+	return tot;
+}
+
+double get_affinities_nonLog(matrix_ll m, int *s, int start)
+{
+   /* results[0] is straight total, results[1] revcomp */
+   int i = 0;
+	double results[2];
+	results[0] = 1;
+	results[1] = 1;
+	while (i < m->length) {
+		results[0] *= m->ll[i][s[start]];
+		results[1] *= m->llrc[i][s[start]]; 
+		i++;
+		start++;
+	}
+	return (results[0] > results[1]) ? results[0] : results[1];
+}
+
+double get_affinities_log(matrix_ll m, int *s, int start)
+{
+	/* results[0] is straight total, results[1] revcomp */
+	int i = 0;
+	double results[2];
+	results[0] = 0;
+	results[1] = 0;
+	while (i < m->length) {
+		results[0] += m->ll[i][s[start]];
+		results[1] += m->llrc[i][s[start]]; 
+		i++;
+		start++;
+	}
+	return (results[0] > results[1]) ? results[0] : results[1];
+}
+
+/*  Function: log2_ratio
+    
+    Returns the log2ratio or only the ratio (depending on info->log2),
+    sets error to 1 if the ratio is non-positive.
+    Computes log2ratio if info->log2 evaluates true, ratio otherwise.
+    
+    Parameters:
+        n - double which will be used as numerator.
+        d - double which will be used as denominator.
+        info - run used to set error flags and determine if log has to be applied.
+        
+    Return:
+        double which is the log2-ratio or simple ratio between n and d.
+        (will be 0 if an error occurred).
+*/
+
+double log2_ratio(double n, double d, run info)
+{
+   if (d < EPSILON) {
+   	info->error = BACKGROUND_FREQ_ERROR;
+		return 0;
+	}
+	double ratio = n / d;
+	if (ratio <= 0) {
+		info->error = BACKGROUND_FREQ_ERROR;
+		return 0;
+	}
+	
+	if (info->log2)
+		return log(ratio) / log(2);
+	else 
+		return ratio;
 }
---
+++ src/total_affinity.h
@@ -0,0 +1,190 @@
+/* total_affinity.h
+//
+// Copyright (C) 2008 Elena Grassi <grassi.e@gmail.com>
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2, or (at your option) any later
+// version.
+//
+// This program is distributed in the hope that it will be useful, but 
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTIBILITY
+// or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+// for more details.
+*/
+
+/*
+    Constants: Matrix and n. of bases
+    
+    BASES - n. of bases, used to load correctly log likelood ratios.
+    MAX_MATRIX_LENGTH - maximum length of used matrixes.
+    MAX_MATRIX_NAME - maximum length of matrixes' names.
+    MAX_MATRIXES - maximum number of matrixes that will be loaded.
+    MAX_FASTA_ID - maximum length of fasta id that will be considered.
+    DEFAULT_WINDOW_SIZE - used if called without -w parameter. 
+                          See run_ struct for further info.
+    DEFAULT_COUNT - used if called without -c parameter.
+    DEFAULT_CUTOFF - used if called without -a parameter.
+    DEFAULT_ABS_CUTOFF - used if called without -p parameter.
+    DEFAULT_REVCOMP - print revcomp as a default if called without -s.
+    DEFAULT_LOG2 - by default it uses likelihoods and not log2-likelihoods 
+                   for total affinity modes (-w != 0), if called without -l.
+    DEFAULT_SLIDE - by default the sliding windows slides 1 by 1. 
+            
+    
+    ERROR - constant used to return an error status, evaluates true (1).
+    OK - constant used to return without error from a function, evaluates false (0).
+    A - constant used to represent DNA base.
+    C - constant used to represent DNA base.
+    G - constant used to represent DNA base.
+    T - constant used to represent DNA base.
+    N - constant used to assign 0 to N when evaluating matches.
+    EPSILON 0.001 - constant used for double equalities evaluation. 
+    EEEPSILON 0.0000000001 - smaller constant used for double equalities evaluation. 
+    COMMAND_LINE_ERROR 1 - internal error code.
+    BACKGROUND_FREQ_ERROR - internal error code.
+    MEMORY_ERROR - internal error code.
+    MATRIX_COUNT_ERROR - internal error code.
+    ILLEGAL_FASTA_CHAR_ERROR - internal error code.
+    MATRIX_LIMIT_ERROR - internal error code.
+    SHORT_FASTA_WARNING - internal error code.
+    MATRIX_FILE_ERROR - internal error code.
+     
+    STARTING_FASTA - initial number of fasta for which storage will be done, dynamic.
+    STARTING_SEQ - initial number of fasta characters for which storage will be done, dynamic.
+    DEBUG - define if you want a lot of debugging infos printed, otherwise comment the defining line.
+*/
+#define BASES 4
+#define MAX_MATRIX_LENGTH 35
+#define MAX_MATRIX_NAME 50
+#define MAX_MATRIXES 2000
+#define MAX_FASTA_ID 10
+#define DEFAULT_COUNT 1 
+#define DEFAULT_LOG2 0 
+#define ERROR 1
+#define OK 0
+#define A 0
+#define C 1
+#define G 2
+#define T 3
+#define N 4
+#define NN 0
+#define EPSILON 0.001 
+#define EEEPSILON 0.0000000001 
+#define COMMAND_LINE_ERROR 1
+#define BACKGROUND_FREQ_ERROR 2
+#define MEMORY_ERROR 3
+#define MATRIX_COUNT_ERROR 4
+#define ILLEGAL_FASTA_CHAR_ERROR 5
+#define MATRIX_LIMIT_ERROR 6
+#define SHORT_FASTA_WARNING 7
+#define MATRIX_FILE_ERROR 8
+#define BG_FILE_ERROR 9
+#define CUTOFF_FILE_ERROR 10
+#define FREQ_ZERO_ERROR 11
+
+#define STARTING_FASTA 50
+#define STARTING_SEQ 17
+/*are we wasting ram? */
+/*#define DEBUG
+#define NDEBUG */
+ 
+#ifndef __TOTAL_AFFINITY_H__
+#define __TOTAL_AFFINITY_H__
+
+#define strong_assert(ASSERTION) ({\
+	if ((ASSERTION)==0){\
+        	fprintf (stderr,\
+			"Assertion failed: " # ASSERTION\
+			", function %s, file %s, line %u.\n",\
+			__func__, __FILE__, __LINE__);\
+			exit(1);\
+	}\
+})
+
+/*
+    Struct: matrix_ll is a pointer to a struct used to store info on matrixes. 
+*/
+typedef struct matrix_ll_ *matrix_ll;
+
+struct matrix_ll_ {
+	double **ll;
+	double **llrc;
+	double **freq;
+	int length;
+	char *name;
+};
+
+
+/*
+    Struct: fasta is a pointer to a struct used to store info on fastas.  
+*/
+typedef struct fas *fasta;
+
+struct fas {
+    int *seq;
+    char *id;
+    double background[BASES];
+    int length;
+    int n_portions;
+};
+
+/* 
+    Struct: run stores informations on this run.
+*/
+typedef struct run_ *run;
+
+struct run_ {
+    int counts; /* if 0 we have fractions, if 1 pseudocounts/counts */
+    int log2; /* if 1 we use log2likelihood even for total affinities mode, if 0 (default) only for ss mode. */
+    double (*get_affinities_pointer)(matrix_ll, int *, int); /* pointer to the function that get affinities */
+    FILE *f_matrixes;
+    FILE *f_fasta;
+    FILE *f_background;
+    int end_mfasta;
+    int n_fasta;
+    char *next_id;
+    matrix_ll *matrixes;
+    int n_matrixes;
+    int error;
+    int normalize_on_seq_len;
+};
+
+void get_matrixes(run info);  
+int find_matrix_index(run info, char *name);
+void get_fractions_from_pcounts(matrix_ll m, run info); 
+void free_matrixes(matrix_ll *m, int loaded);
+int alloc_matrixes(matrix_ll **m);  
+
+double matrix_little_window_tot(matrix_ll m, fasta s, int begin, run info); 
+double get_affinities_nonLog(matrix_ll m, int *s, int start);
+double get_affinities_log(matrix_ll m, int *s, int start);
+void assign_ll(matrix_ll m, double *bg, run info);
+double log2_ratio(double n, double d, run info);
+
+void free_fasta(run info);
+fasta load_next_fasta(run info, double *bg);
+void add_count_bg(fasta f, char c, run info); 
+int encode_base(char c);
+int encoded_rc(int n);
+void assign_zero_bg(fasta f); 
+void get_background_from_file(run info, double *bg);
+void assign_bg_from_file(fasta f, double *bg);
+void get_id(FILE *f, char **s, run info);
+void skip_line(FILE *f);    
+void get_seq(char *seq, int offset, int len, char *match);
+void get_rc(char *seq, int offset, int len, char *match); 
+
+void check_error(run info);
+int is_atofable(char *s);
+int is_atoable(char *s);
+int open_file(char *argv[], int position, FILE **to_open);
+void parse_command_line(char *argv[], int argc, run info);
+void check_parameters(run info);
+char *get_next_token(char *argv[], int argc, int next_token);
+void tidy(run info);
+int set_beginning_info(run *info);
+
+void print_fasta(fasta f);
+void print_matrixes(run info);
+#endif
