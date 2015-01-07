1ea8d92a1d00812dcbecee23a726bea4981e3108
Elena Grassi <grassi.e@gmail.com>
2015-01-07 16:07:29 +0100

    TFSBTools-PWMatrix S4 access in C

--- R/matrix_rider_methods.R
+++ R/matrix_rider_methods.R
@@ -1,8 +1,7 @@
-setMethod("getSeqOccupancy", signature=(pwm="integer"),
+setMethod("getSeqOccupancy", signature=(pwm="PWMatrix"),
    function(pwm, subject, seqname="Unknown", min.score=0.8) {
       c_res <- .Call("get_occupancy", pwm)
-      cat(c_res)
-      cat("\n")
+      print(c_res)
       return(c_res)
    }
 )
--- src/matrix_rider.c
+++ src/matrix_rider.c
@@ -1,5 +1,6 @@
 #include <R.h>
 #include <Rinternals.h>
+#include <Rdefines.h>
 
 SEXP get_occupancy(SEXP);
 int factorial(int);
@@ -7,33 +8,26 @@ int rec_fact(int);
 
 SEXP get_occupancy(SEXP pwm) 
 {
-   int local_pwm = Rf_asInteger(pwm);
+   //SEXP matrix_slot = mkChar("profileMatrix\0");
+   //PrintValue(matrix_slot);
+ 
+   // SEXP dim = GET_DIM(pwm); // --> this is NULL for my pwm. Why?
+   SEXP mat = GET_SLOT(pwm, install("profileMatrix"));
+   int ncol = INTEGER(GET_DIM(mat))[0];
+   int nrow = INTEGER(GET_DIM(mat))[1];
+   // IFDEF DEBUG
+   Rprintf("ncol %d\n", ncol);
+   Rprintf("nrow %d\n", nrow);
+   //PrintValue(mat);
+   // ENDIF
    
-   //SEXP res = PROTECT(Rf_allocSExp(INTSXP));
-   //*(INTEGER(res)) = factorial(local_pwm);
-   
-   SEXP res = PROTECT(allocVector(INTSXP,1));
-   int * p = INTEGER(res);
-   p[0] = factorial(local_pwm);
-   
+   SEXP res = PROTECT(allocVector(INTSXP,3));
+   int *p = INTEGER(res);
+   p[0] = ncol;
+   p[1] = nrow;
+   int i = 0;
+   int j = 2;
+   p[2] = (int)(REAL(mat)[i*nrow+j]); // access element at row i and col j (0 based)
    UNPROTECT(1);
    return(res);
-}
-
-int factorial(int x)
-{
-   if (x < 1) {
-      return 0;
-   } else {
-      return rec_fact(x);
-   }
-}
-
-int rec_fact(int x)
-{
-   if (x == 1) {
-      return 1;
-   } else {
-      return x*rec_fact(x-1);
-   }
 }
