a43e0cc85b410d174bddf59aa883729e7ccdac03
Elena Grassi <grassi.e@gmail.com>
2015-01-21 13:16:52 +0100

    Skeleton for RUnit tests and camelCase fix

--- R/matrix_rider_methods.R
+++ R/matrix_rider_methods.R
@@ -1,15 +1,14 @@
 setMethod("getSeqOccupancy", signature=c(sequence="DNAString", pfm="PFMatrix", cutoff="numeric"),
    function(sequence, pfm, cutoff) {
    # DNAString, XStringViews or MaskedDNAString
-   c_res <- .Call("get_occupancy", pfm, cutoff, sequence)
-   return(c_res)
+   cRes <- .Call("get_occupancy", pfm, cutoff, sequence)
+   return(cRes)
    }
 )
 
 setMethod("getSeqOccupancy", signature=c(sequence="DNAString", pfm="PFMatrixList", cutoff="numeric"),
    function(sequence, pfm, cutoff){
-      ans_list = lapply(pfm, getSeqOccupancy, cutoff=cutoff, sequence=sequence)
-      #ans = do.call(SiteSetList, ans_list)
-      return(unlist(ans_list))
+      ansList = lapply(pfm, getSeqOccupancy, cutoff=cutoff, sequence=sequence)
+      return(unlist(ansList))
    }
 )
---
+++ inst/unitTests/test_getSeqOccupancy.R
@@ -0,0 +1,8 @@
+# library(RUnit)
+# source('inst/unitTests/test_getSeqOccupancy.R')
+# test_getSeqOccupancy()
+test_getSeqOccupancy <- function() {
+   checkEquals(2, 2)
+   #checkTrue(is.na(divideBy(4, 0)))
+   #checkEqualsNumeric(divideBy(4, 1.2345), 3.24, tolerance=1.0e-4)
+}
